d0c3287508ae163569c862ba047f5f24a7a35ca6
Brian <italissimo@gmail.com>
2016-08-11 18:26:25 +1000

    adds webpack sass-loader and css-loader, basic page structure

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,15 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	module: {
-		loaders: [{ 
-			test: /\.js$/,
-			loader: 'babel'
-		}]
-	}
+		loaders: [
+			{ test: /\.js$/, loader: 'babel'},
+			{ test: /\.scss$/, loaders: ["style", "css", "sass"] }
+		]
+	},
+	resolve: {
+        extensions: ['', '.js', '.jsx', '.css'],
+        modulesDirectories: [
+          'node_modules'
+        ]
+    }
 };
