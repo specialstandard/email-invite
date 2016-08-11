3c9e0a7f78f3369879e82a725e01c35d6bb6a73f
Brian <italissimo@gmail.com>
2016-08-11 22:36:34 +1000

    app validates input, shows loading message, and shows success message

--- src/app/api-service/api-service.js
+++ src/app/api-service/api-service.js
@@ -4,8 +4,18 @@ export default class ApiService {
     }
 
 	// Email Invite service function
-	getData () {
-		return this.$http({method: 'GET', url: './api' });
+	postData ( data ) {
+		const url = "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth";
+		this.$http.post( url , data).success(function(response, status) {
+            //this.data = data;
+						//this.status = status;
+						console.log('response: ', response )
+						console.log('status: ', status )
+						return {
+							response: response,
+							status: status
+						}
+        })
 	}
 }
 
--- src/app/invite-component/invite-directive/invite-directive.js
+++ src/app/invite-component/invite-directive/invite-directive.js
@@ -21,14 +21,79 @@ export default class InviteDirective {
 
 // Directive's controller
 class InviteDirectiveController {
-    constructor () {
-        this.showModal = false;
+    constructor (apiService, $timeout, $http, $q) {
+        //this.apiService = apiService;
+        this.$http = $http
+        this.showRequestModal = false;
+        this.showSuccessModal = false;
+        this.requestProcessing = false;
+        this.user = {
+            name:'',
+            email:'',
+            emalConf: ''
+        }
+        this.apiObj = {
+            response:'',
+            status:''
+        }
         this.onClickRequest = () => {
             console.log('pushed')
-            this.showModal = true;
+            this.showRequestModal = true;
         }
-        this.onClickCloseModal = () => {
-            this.showModal = false;
+        this.onClickCloseRequestModal = () => {
+            this.showRequestModal = false;
+        }
+        this.onClickCloseSuccessModal = () => {
+            this.showSuccessModal = false;
+
+        }
+        this.onClickSend = () => {
+            let self = this;
+
+            console.log('clicked send')
+            let bValid = true
+            if ( self.user.name.length < 3 ){
+                bValid = false
+            } else if ( self.requestForm.userEmail.$invalid ){
+                bValid = false
+            } else if ( self.requestForm.userEmailConf.$invalid ){
+                bValid = false
+            } else if ( self.user.email !== self.user.emailConf ){
+                bValid = false
+            }
+
+            if ( bValid ){
+                //$timeout(()=>{
+                    this.requestProcessing = true
+                //})
+                this.data = {
+                    name: self.user.name,
+                    email: self.user.email
+                }
+                self.user.name = ''
+                self.user.email = ''
+                self.user.emailConf = ''
+
+                this.data = JSON.stringify( this.data );
+                const url = "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth";
+                this.$http.post( url , this.data).success((response, status) =>{
+
+                    console.log('response: ', response )
+                    console.log('status: ', status )
+                    if ( status === 200 ){
+                        self.showRequestModal = false;
+                        self.showSuccessModal = true;
+                        this.requestProcessing = false
+                    }
+                })
+                .failure((response, status) =>{
+
+                    console.log('response: ', response )
+                    console.log('status: ', status )
+                    this.requestProcessing = false
+
+                })
+            }
         }
     }
 }
